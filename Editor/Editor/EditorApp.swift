07a1d09afba2a06c283330556f47c8a8638ae3dc
yazzgul <yazgul.khasanshina@gmail.com>
2025-05-07 13:23:49 +0300

    add project

---
+++ Editor/Editor/EditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  EditorApp.swift
+//  Editor
+//
+//  Created by Язгуль Хасаншина on 07.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct EditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
